c6a80b0f95928ef9b3514eda8a20ff01163aac36
areumsheep <areumsheep@gmail.com>
2023-01-23 19:08:03 +0900

    Update: Callout의 img는 무조건 원 모양이 되도록 스타일 변경

--- src/components/@common/Callout/Callout.styles.ts
+++ src/components/@common/Callout/Callout.styles.ts
@@ -4,13 +4,17 @@ export const Callout = styled.div`
   display: flex;
   align-items: center;
   border-radius: 20px;
-  padding: 20px;
+  padding: 18px 20px;
   margin-bottom: 25px;
   background-color: ${({ theme }) => theme.color.livid50};
 `;
 
 export const Icon = styled.div`
   margin-right: 12px;
+
+  img {
+    border-radius: 50%;
+  }
 `;
 
 export const Description = styled.div``;
