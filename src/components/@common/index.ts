40a7829f4b022e7837ee41a38f4251934430927f
areumsheep <areumsheep@gmail.com>
2023-02-01 02:48:00 +0900

    Update: 공통 컴포넌트 import 한 번에 가능하도록 export 구문 추가

---
+++ src/components/@common/index.ts
@@ -0,0 +1,5 @@
+export { Anchor } from 'components/@common/Anchor';
+export { Callout } from 'components/@common/Callout';
+export { Flex } from 'components/@common/Flex';
+export { Stack } from 'components/@common/Stack';
+export { Typography } from 'components/@common/Typography';
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Layout } from 'components/Layout';
+export { default as MdxContent } from 'components/MdxContent';
+export { default as Seo } from 'components/Seo';
