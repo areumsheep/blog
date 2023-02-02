4eb150347e964bba57482e04b6b20ba7ef93e550
areumsheep <areumsheep@gmail.com>
2023-02-03 00:59:23 +0900

    Add: Chip 스토리북 추가

---
+++ src/components/@common/Chip/Chip.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type { ComponentStory } from '@storybook/react';
+import styled from '@emotion/styled';
+
+import Chip from './Chip';
+
+export default {
+  title: 'Common/Chip',
+  component: Chip,
+  argTypes: {
+    color: {
+      table: {
+        disable: true,
+      },
+    },
+    backgroundColor: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const Primary: ComponentStory<typeof Chip> = (args) => <Chip {...args}>JavaScript</Chip>;
+
+Primary.args = {
+  color: 'black',
+  backgroundColor: 'livid200',
+};
